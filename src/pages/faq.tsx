6c29777de30e9654364641f8ef8b5a1c13daa036
Fillipe Augusto <fillipeaugustoribeiro@gmail.com>
2022-04-13 01:58:50 -0300

    testing

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -11,7 +11,7 @@ export default function Faq() {
 			<section className="relative py-16 bg-white min-w-screen animation-fade animation-delay">
 				<div className="container px-0 px-8 mx-auto sm:px-12 xl:px-5">
 					<p className="text-xs font-bold text-left text-pink-500 uppercase sm:mx-6 sm:text-center sm:text-normal sm:font-bold">
-						Tem alguma dúvida ? encontre a resposta aqui.
+						Tem alguma dúvida ? encontre a resposta aqui .
 					</p>
 					<h3 className="mt-1 text-2xl font-bold text-left text-gray-800 sm:mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
 						Perguntas frequentes
